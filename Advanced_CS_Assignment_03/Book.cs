036463c3e5496826f1a4f966b0d1f909d3512b91
135ahmedsayed <ahmedshkra246@gmail.com>
2025-08-02 18:12:36 +0300

    Add project files.

---
+++ Advanced_CS_Assignment_03/Book.cs
@@ -0,0 +1,33 @@
+﻿namespace Advanced_CS_Assignment_03;
+public class Book
+{
+
+    public string ISBN { get; set; }
+    public string Title { get; set; }
+    public string[] Authors { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public decimal Price { get; set; }
+    public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+    {
+        throw new NotImplementedException();
+    }
+    public override string ToString()
+    {
+        throw new NotImplementedException();
+    }
+}
+public class BookFunctions
+{
+    public static string GetTitile(Book B)
+    {
+        throw new NotImplementedException();
+    }
+    public static string GetAuthors(Book B)
+    {
+        throw new NotImplementedException();
+    }
+    public static string GetPrice(Book B)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Advanced_CS_Assignment_03/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Advanced_CS_Assignment_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
